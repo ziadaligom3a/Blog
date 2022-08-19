09ed1e27079e7578641eff42224cf25ec3d7348e
Ziad Ali (ZIX) <111474065+ziadaligom3a@users.noreply.github.com>
2022-08-19 23:53:38 +0200

    Update posts-featured-card.blade.php

--- resources/views/components/posts/posts-featured-card.blade.php
+++ resources/views/components/posts/posts-featured-card.blade.php
@@ -1,5 +1,5 @@
 @props(['post'])
-@dd($post)
+
 <article    
 class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
 <div class="py-6 px-5 lg:flex">
